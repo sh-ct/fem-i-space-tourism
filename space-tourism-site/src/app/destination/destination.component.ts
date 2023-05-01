d6fa1a81f45a88292a68d79eb0045f69e48b17e3
sh-ct <s1.turner@outlook.com>
2023-05-01 20:58:13 +0100

    initial page set up basic html and data

--- space-tourism-site/src/app/destination/destination.component.ts
+++ space-tourism-site/src/app/destination/destination.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import jsonData from '../../assets/json/data.json';
+import { IDestination } from "./destination.model";
 
 @Component({
   selector: 'app-destination',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./destination.component.scss']
 })
 export class DestinationComponent {
+  data: IDestination[] = jsonData.destinations;
+  index: number = 0;
+
+  stepIndex() {
+    if (this.index + 1 > this.data.length) {
+      this.index = 0;
+    } else {
+      ++this.index;
+    }
+  }
+
+  get destination() {
+    return this.data[this.index];
+  }
 
 }
---
+++ space-tourism-site/src/app/destination/destination.model.ts
@@ -0,0 +1,10 @@
+export interface IDestination {
+  name: string;
+  images: {
+    png: string,
+    webp: string
+  };
+  description: string;
+  distance: string;
+  travel: string;
+}
