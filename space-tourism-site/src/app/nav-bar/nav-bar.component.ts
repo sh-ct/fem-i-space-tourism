0956350d7f4a8548c7c38d7b7a5d7ce8bfaae3f2
sh-ct <s1.turner@outlook.com>
2023-05-31 17:34:24 +0100

    added landscape layout for technology page for mobile nad tablet and updated nav bars to show an active state for the current page and added animation to the destination planet nav bar

--- space-tourism-site/src/app/nav-bar/nav-bar.component.ts
+++ space-tourism-site/src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ITab } from "../models/tab.interface";
 import { tabs } from "./nav-bar-tabs.model";
+import { page } from "../background.service";
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,4 +10,8 @@ import { tabs } from "./nav-bar-tabs.model";
 })
 export class NavBarComponent {
   tabs: ITab[] = tabs;
+
+  get url(): string {
+    return window.location.href;
+  }
 }
